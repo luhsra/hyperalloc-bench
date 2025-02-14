2c56f7d8502cd5aaf6a95e0259c070edf52b0bf4
Lars Wrenger <wrenger@sra.uni-hannover.de>
2025-02-14 18:48:58 +0100

    Docker use internal port

--- artifact-eval/run.sh
+++ artifact-eval/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --network=host --device=/dev/kvm $@ --rm ghcr.io/luhsra/hyperalloc_ae
+docker run -p 127.0.0.1:2222:2222 --device=/dev/kvm $@ --rm ghcr.io/luhsra/hyperalloc_ae
